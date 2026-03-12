3bd71bd9b6ef454025fac854a0a12dd92aca9cda
Jessica Schouten <jessica.schouten@live.nl>
2026-03-12 15:28:16 +0100

    Advent of code 2025 dag 1 puzzel 2.

--- src/main/java/com/adventofcode/aoc2025/Dag01.java
+++ src/main/java/com/adventofcode/aoc2025/Dag01.java
@@ -10,25 +10,30 @@ public class Dag01 {
     public static final int START_CIJFER = 50;
 
     int pijl = START_CIJFER;
-    int aantalNullen = 0;
+
+    // Alleen eindstand per draai
+    int aantalEindstandNullen = 0;
+
+    // Alle nullen door clicks (onderweg + eindstap)
+    int totaalAantalNullen = 0;
 
     public static void main(String[] args) throws IOException {
         Dag01 dag01 = new Dag01();
 
         Path pad = Path.of("src/main/resources/aoc2025/dag01.txt");
-        dag01.telAantalNullen(pad);
+        dag01.verwerkBestand(pad);
 
         System.out.println("Eindstand pijl: " + dag01.pijl);
-        System.out.println("Nullen geteld: " + dag01.aantalNullen);
+        System.out.println("Nullen als eindstand (per draai): " + dag01.aantalEindstandNullen);
+        System.out.println("Totaal van alle nullen (onderweg + eindstap): " + dag01.totaalAantalNullen);
     }
 
-    protected int telAantalNullen(Path pad) throws IOException {
+    protected void verwerkBestand(Path pad) throws IOException {
         try (var regels = Files.lines(pad, StandardCharsets.UTF_8)) {
             regels.map(String::trim)
                     .filter(s -> !s.isEmpty())
                     .forEach(this::verwerkRegel);
         }
-        return pijl;
     }
 
     void verwerkRegel(String regel) {
@@ -43,14 +48,41 @@ public class Dag01 {
     }
 
     protected void draaiNaarRechts(int beginCijfer, int rCijfer) {
-        // Altijd 0-99
+        telNullenTijdensDraaiRechts(beginCijfer, rCijfer);
+
         pijl = Math.floorMod(beginCijfer + rCijfer, 100);
-        if (pijl == 0) aantalNullen++;
+        if (pijl == 0) aantalEindstandNullen++;
     }
 
     protected void draaiNaarLinks(int beginCijfer, int lCijfer) {
-        // Altijd 0-99
+        telNullenTijdensDraaiLinks(beginCijfer, lCijfer);
+
         pijl = Math.floorMod(beginCijfer - lCijfer, 100);
-        if (pijl == 0) aantalNullen++;
+        if (pijl == 0) aantalEindstandNullen++;
+    }
+
+    protected void telNullenTijdensDraaiRechts(int beginCijfer, int rCijfer) {
+        if (rCijfer <= 0) return;
+
+        // clicks: begin+1 .. begin+r (inclusief eindstap)
+        int min = beginCijfer + 1;
+        int max = beginCijfer + rCijfer;
+        totaalAantalNullen += telHonderdtallenBinnenBereik(min, max);
+    }
+
+    protected void telNullenTijdensDraaiLinks(int beginCijfer, int lCijfer) {
+        if (lCijfer <= 0) return;
+
+        // clicks: begin-1 .. begin-l (inclusief eindstap)
+        int min = beginCijfer - lCijfer;
+        int max = beginCijfer - 1;
+        totaalAantalNullen += telHonderdtallenBinnenBereik(min, max);
+    }
+
+    private static int telHonderdtallenBinnenBereik(int min, int max) {
+        if (min > max) return 0;
+
+        // Aantal veelvouden van 100 in [min..max], werkt ook voor negatieve grenzen.
+        return Math.floorDiv(max, 100) - Math.floorDiv(min - 1, 100);
     }
 }
