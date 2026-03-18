720dc83cfb3da4a6e150045d3ee86f56dc8b6941
nmcb <nmcb@xs4all.nl>
2026-03-18 15:47:36 +0100

    pass operators as arguments to reduce \code duplication

--- src/main/java/com/adventofcode/aoc2025/Dag01.java
+++ src/main/java/com/adventofcode/aoc2025/Dag01.java
@@ -1,5 +1,7 @@
 package com.adventofcode.aoc2025;
 
+import java.util.function.BinaryOperator;
+
 public class Dag01 {
 
     public static final int START_CIJFER = 50;
@@ -12,46 +14,33 @@ public class Dag01 {
     // Alle nullen door clicks (onderweg + eindstap)
     int totaalAantalNullen = 0;
 
+    static BinaryOperator<Integer> rechts = (Integer a, Integer b) -> a + b;
+    static BinaryOperator<Integer> links  = (Integer a, Integer b) -> a - b;
+
     void verwerkRegel(String regel) {
         char richting = regel.charAt(0);
         int aantal = Integer.parseInt(regel.substring(1).trim());
 
         switch (richting) {
-            case 'L' -> draaiNaarLinks(pijl, aantal);
-            case 'R' -> draaiNaarRechts(pijl, aantal);
+            case 'L' -> draaiNaar(links, pijl, aantal);
+            case 'R' -> draaiNaar(rechts, pijl, aantal);
             default -> throw new IllegalArgumentException("Onbekende regel: " + regel);
         }
     }
 
-    protected void draaiNaarRechts(int beginCijfer, int rCijfer) {
-        telNullenTijdensDraaiRechts(beginCijfer, rCijfer);
+    protected void draaiNaar(BinaryOperator<Integer> richting, int beginCijfer, int rCijfer) {
+        telNullenTijdensDraaiNaar(richting, beginCijfer, rCijfer);
 
-        pijl = Math.floorMod(beginCijfer + rCijfer, 100);
+        pijl = Math.floorMod(richting.apply(beginCijfer, rCijfer), 100);
         if (pijl == 0) aantalEindstandNullen++;
     }
 
-    protected void draaiNaarLinks(int beginCijfer, int lCijfer) {
-        telNullenTijdensDraaiLinks(beginCijfer, lCijfer);
-
-        pijl = Math.floorMod(beginCijfer - lCijfer, 100);
-        if (pijl == 0) aantalEindstandNullen++;
-    }
-
-    protected void telNullenTijdensDraaiRechts(int beginCijfer, int rCijfer) {
+    protected void telNullenTijdensDraaiNaar(BinaryOperator<Integer> richting, int beginCijfer, int rCijfer) {
         if (rCijfer <= 0) return;
 
         // clicks: begin+1 .. begin+r (inclusief eindstap)
-        int min = beginCijfer + 1;
-        int max = beginCijfer + rCijfer;
-        totaalAantalNullen += telHonderdtallenBinnenBereik(min, max);
-    }
-
-    protected void telNullenTijdensDraaiLinks(int beginCijfer, int lCijfer) {
-        if (lCijfer <= 0) return;
-
-        // clicks: begin-1 .. begin-l (inclusief eindstap)
-        int min = beginCijfer - lCijfer;
-        int max = beginCijfer - 1;
+        int min = richting.apply(beginCijfer, 1);
+        int max = richting.apply(beginCijfer, rCijfer);
         totaalAantalNullen += telHonderdtallenBinnenBereik(min, max);
     }
 
