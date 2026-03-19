3b51a21ffaa7bb73e2f648a83e597ce4f0cfa6e2
nmcb <nmcb@xs4all.nl>
2026-03-19 18:05:44 +0100

    cleanup

--- src/main/java/com/adventofcode/aoc2025/Dag02.java
+++ src/main/java/com/adventofcode/aoc2025/Dag02.java
@@ -1,5 +1,7 @@
 package com.adventofcode.aoc2025;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -7,7 +9,11 @@ import java.util.stream.Stream;
 import static java.lang.Long.parseLong;
 import static java.util.stream.Stream.iterate;
 
+@Getter
 public class Dag02 {
+
+    private long answerPart1 = 0;
+    private long answerPart2 = 0;
 
     public static class Range
     {
@@ -24,10 +30,7 @@ public class Dag02 {
         }
     }
 
-    private long answerPart1 = 0;
-    private long answerPart2 = 0;
-
-    protected List<Range> parseInput(String input) {
+    private List<Range> parseInput(String input) {
         List<Range> ranges = new ArrayList<>();
         for (String range : input.trim().split("\\s*,\\s*")) {
             String[] boundary = range.trim().split("\\s*-\\s*");
@@ -36,30 +39,22 @@ public class Dag02 {
         return ranges;
     }
 
-    static protected boolean isRepeatedTwice(Long number) {
+    static private boolean isRepeatedTwice(Long number) {
         String test = number.toString();
-        String links = test.substring(0, test.length() / 2);
-        String rechts = test.substring(test.length() / 2);
-        return test.length() % 2 == 0 && links.equals(rechts);
+        String left = test.substring(0, test.length() / 2);
+        String right = test.substring(test.length() / 2);
+        return test.length() % 2 == 0 && left.equals(right);
     }
 
-    static protected boolean isRepeatedAtLeastTwice(Long number) {
+    static private boolean isRepeatedAtLeastTwice(Long number) {
         String test = number.toString();
         String extended = test + test;
         return extended.substring(1, extended.length() - 1).contains(test);
     }
 
-    protected void verwerkRegel(String line) {
+    public void verwerkRegel(String line) {
         List<Range> ranges = parseInput(line);
         answerPart1 = ranges.stream().flatMap(Range::stream).filter(Dag02::isRepeatedTwice).mapToLong(Long::longValue).sum();
         answerPart2 = ranges.stream().flatMap(Range::stream).filter(Dag02::isRepeatedAtLeastTwice).mapToLong(Long::longValue).sum();
-    }
-
-    protected long getAnswerPart1() {
-        return answerPart1;
-    }
-
-    protected long getAnswerPart2() {
-        return answerPart2;
     }
 }
