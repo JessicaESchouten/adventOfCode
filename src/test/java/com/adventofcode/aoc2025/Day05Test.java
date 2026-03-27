c42d540f89861cf7d775470d70f77c116274a915
Jessica Schouten <jessica.schouten@live.nl>
2026-03-27 09:24:57 +0100

    feat(day05): add puzzle 2

--- src/test/java/com/adventofcode/aoc2025/Day05Test.java
+++ src/test/java/com/adventofcode/aoc2025/Day05Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class Day05Test {
 
     @Test
-    void countsFreshIds_puzzleExample() {
+    void countFreshAvailableIds_exampleFromPuzzle() {
         String input = """
                 3-5
                 10-14
@@ -21,25 +21,45 @@ class Day05Test {
                 32
                 """;
 
-        // Fresh ids here: 5 (3-5), 11 (10-14), 17 (16-20 and 12-18) => 3 total
-        Assertions.assertEquals(3L, Day05.countFreshIds(input));
+        Assertions.assertEquals(3L, Day05.countFreshAvailableIds(input));
     }
 
     @Test
-    void countsFreshIds_puzzleExample_crlfInput() {
-        String input =
-                "3-5\r\n" +
-                "10-14\r\n" +
-                "16-20\r\n" +
-                "12-18\r\n" +
-                "\r\n" +
-                "1\r\n" +
-                "5\r\n" +
-                "8\r\n" +
-                "11\r\n" +
-                "17\r\n" +
-                "32\r\n";
+    void countDistinctFreshIdsInRanges_exampleFromPuzzle() {
+        String input = """
+                3-5
+                10-14
+                16-20
+                12-18
 
-        Assertions.assertEquals(3L, Day05.countFreshIds(input));
+                1
+                5
+                8
+                11
+                17
+                32
+                """;
+
+        Assertions.assertEquals(14L, Day05.countDistinctFreshIdsInRanges(input));
+    }
+
+    @Test
+    void handlesCrLfBlankLineSeparator_exampleFromPuzzle() {
+        String input = """
+                3-5
+                10-14
+                16-20
+                12-18
+
+                1
+                5
+                8
+                11
+                17
+                32
+                """.replace("\n", "\r\n");
+
+        Assertions.assertEquals(3L, Day05.countFreshAvailableIds(input));
+        Assertions.assertEquals(14L, Day05.countDistinctFreshIdsInRanges(input));
     }
 }
